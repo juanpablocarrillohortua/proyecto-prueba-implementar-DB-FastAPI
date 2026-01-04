58ddfce71fcbb655daee0b6e9ed4354cd784021a
Juan Pablo Carrillo Hortua <juanpablocarrillohortua@gmail.com>
2026-01-04 15:23:21 -0500

    url fixed

--- public/formulary.js
+++ public/formulary.js
@@ -13,7 +13,7 @@ form.addEventListener('submit', async (e) => {
 
     try {
         // Enviamos los datos a la API (ajusta la URL si es necesario)
-        const response = await fetch('https://proyecto-prueba-implementar-db-fastapi.onrender.com/heroes', {
+        const response = await fetch('https://proyecyo-prueba-implementar-db-fastapi.onrender.com', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(heroData)
